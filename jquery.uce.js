d5c587f8c7308ca42ebcb9c5278b69e0ce6706f6
Johannes Donath <akkarin@develfusion.com>
2011-05-27 18:17:31 +0200

    Let's code a fucking plugin (The "fucking" was just for our statistics ;-P)

---
+++ jquery.uce.js
@@ -0,0 +1,15 @@
+/*!
+ * jQuery UNIX Console Emulator Plugin
+ * Copyright 2011, Johannes Donath
+ * Licensed under the GNU Lesser Public License
+*/
+
+(function(jQuery){
+	$.fn.terminal = function(options) {
+		var defaultOptions = {
+			'callbackUrl'			:	'terminal.php',
+			'callbackParameter'		:	'command'
+		};
+		
+	}
+})( jQuery );
